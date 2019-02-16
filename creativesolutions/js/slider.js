57a8e4af17967f90be3fbc3031fddaf2ada1eea4
anastasija tupichka <tupichka.anastasija@ukr.net>
2019-02-16 16:38:19 +0200

    exam3

---
+++ creativesolutions/js/slider.js
@@ -0,0 +1,15 @@
+    $(document).ready(function(){
+      $('.slider').slick({
+        arrows: true,
+        nextArrow: '._nextArrow',
+        prevArrow: '._prevArrow',
+        dots: true
+      });
+      // $('.reviews-slider').slick({
+      //   dots: true,
+      //   arrows: false,
+      //   slidesToShow: 1,
+      //   autoplay: true,
+      //   autoplaySpeed: 6000,
+      // });
+    });
